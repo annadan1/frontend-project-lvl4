ad895818c28fe9102b42d07a6fba90ee6c26ec16
Anna <anna_s92_@mail.ru>
2022-05-06 13:32:44 +0400

    Add validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,60 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import React from 'react';
-import Login from './components/Login.jsx';
-import Homepage from './components/Homepage.jsx';
-import Notfound from './components/Notfound.jsx';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+import Login from './components/LoginPage.jsx';
+import ChatPage from './components/ChatPage.jsx';
+import Notfound from './components/NotfoundPage.jsx';
+import AuthContext from './contexts/index.jsx';
+import useAuth from './hooks/index.jsx';
+
+function AuthProvider({ children }) {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function PrivateRoute({ children }) {
+  const auth = useAuth();
+
+  return auth.loggedIn ? children : <Navigate to="/login" />;
+}
 
 function App() {
   return (
-    <>
-      <header>
-        <Link to="/" />
-        <Link to="/login" className="navbar-brand">Hexlet Chat</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </>
+    <AuthProvider>
+      <div className="d-flex flex-column h-100">
+        <Navbar expand="lg" bg="white" variant="light" className="shadow-sm">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              Hexlet Chat
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <PrivateRoute>
+                <ChatPage />
+              </PrivateRoute>
+            )}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,107 @@
+import axios from 'axios';
+import * as yup from 'yup';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Form, Card } from 'react-bootstrap';
+import useAuth from '../hooks/index.jsx';
+import routes from '../routes.js';
+
+const formSchema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
+
+function Login() {
+  const [authFailed, setAuthFailed] = useState(false);
+  const navigate = useNavigate();
+  const auth = useAuth();
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validation: { formSchema },
+    onSubmit: async (values) => {
+      setAuthFailed(false);
+      try {
+        const res = await axios.post(routes.loginPath(), values);
+        localStorage.setItem(values.username, JSON.stringify(res.data));
+        auth.logIn();
+        navigate('/');
+      } catch (err) {
+        if (err.isAxiosError && err.response.status === 401) {
+          setAuthFailed(true);
+          inputRef.current.select();
+          return;
+        }
+        throw err;
+      }
+    },
+  });
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-4 col-xxl-3">
+          <Card className="shadow-sm">
+            <Card.Body className="row p-5 justify-content-center">
+              <Form onSubmit={formik.handleSubmit}>
+                <h1 className="text-center mb-4">Войти</h1>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    name="username"
+                    autoComplete="username"
+                    placeholder="Ваш ник"
+                    id="username"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    isInvalid={authFailed}
+                    ref={inputRef}
+                  />
+                  <Form.Label>Ваш ник</Form.Label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    name="password"
+                    autoComplete="current-password"
+                    placeholder="Пароль"
+                    id="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    isInvalid={authFailed}
+                  />
+                  <Form.Label>Пароль</Form.Label>
+                  <Form.Control.Feedback type="invalid">
+                    Неверные имя пользователя или пароль
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                  type="submit"
+                  variant="outline-primary"
+                  className="w-100 mb-3"
+                >
+                  Войти
+                </Button>
+              </Form>
+            </Card.Body>
+            <Card.Footer className=" p-4">
+              <Card.Text className="text-center">
+                <span>Нет аккаунта? </span>
+                <Link to="/signup">Регистрация</Link>
+              </Card.Text>
+            </Card.Footer>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
