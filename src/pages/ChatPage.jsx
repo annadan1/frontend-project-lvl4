8393bf8ffca12182bb4537d853996de26a4fecdf
Anna <anna_s92_@mail.ru>
2022-05-24 15:21:54 +0400

    Add user

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -39,10 +39,7 @@ function ChatPage() {
           <div className="d-flex flex-column h-100">
             <div className="bg-light mb-4 p-3 shadow-sm small">
               <p className="m-0">
-                <b>
-                  #
-                  {currentChannel?.name}
-                </b>
+                <b>{`# ${currentChannel?.name}`}</b>
               </p>
               <span className="text-muted">
                 {t('messages.message', {
